780d61100102494aa15fc11b11a7c9a0d470103e
Patrick <patrick.roessler@dci-student.org>
2024-09-18 12:43:04 +0200

    Updated App.jsx and added package-lock.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import Shopmen from './components/ShowMen.jsx';
 import ShopWomen from './components/ShowWomen.jsx';
 import Header from "./components/Header";
@@ -9,13 +8,12 @@ function App() {
 
   return (
     <>
-     <ShopWomen />
-     <Shopmen />
      <div className="flex flex-col min-h-screen">
       {/* Header */}
       <Header />
 
-      {/* Main Content - flex-grow to push the footer to the bottom */}
+      <ShopWomen />
+      <Shopmen />
       <main className="flex-grow">{/* Your main content goes here */}</main>
 
       {/* Footer - this will always stay at the bottom */}
