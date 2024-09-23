d573e8818389b50acccfec6cddfc88d72c61c5e1
Patrick <patrick.roessler@dci-student.org>
2024-09-23 12:03:17 +0200

    Datenbank

---
+++ backend/db/connection.db.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+export async function mongoConnect() {
+  try {
+    await mongoose.connect(process.env.CONNECTION_STRING, {
+      dbName: "ampes",
+    });
+    console.log("Connection zu MongoDB erfolgreich");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export function mongoErrorListener() {
+  mongoose.connection.on("error", (err) => {
+    console.log(err);
+  });
+}
+
+export function mongoDCListener() {
+  mongoose.connection.on("disconnected", (err) => {
+    console.log(err);
+  });
+}
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,30 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import {
+    mongoConnect,
+    mongoDCListener,
+    mongoErrorListener,
+  } from "./db/connection.db.js";
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`Server läuft auf Port ${PORT}`);
-});
+dotenv.config();
 
+mongoDCListener();
+mongoErrorListener();
+await mongoConnect();
+
+app.all("*", (req, res, next) => {
+    next(createError.NotFound("This Page is not found"));
+  });
+  
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+      code: err.status,
+      answer: err.message || "Servererror Contact Support",
+    });
+  });
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server läuft auf Port ${PORT}`);
+  });
