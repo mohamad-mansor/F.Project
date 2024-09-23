87dba26924e46c82038829ab1f1d7b4f71582841
Patrick <patrick.roessler@dci-student.org>
2024-09-23 15:24:13 +0200

    update

---
+++ backend/models/User.model.js
@@ -0,0 +1,52 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+export const UserSchema = new Schema({
+  username: {
+    type: String,
+    minLength: 3,
+    maxLength: 16,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    minLength: 8,
+    maxLength: 16,
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+  },
+  verified: Number,
+  verifylink: String,
+  tos: {
+    type: Number,
+    enum: [1],
+  },
+});
+
+// Unser Return-Objekt aufräumen
+UserSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  delete obj.__v;
+  return obj;
+};
+
+// Passwort wird mit dem gehasten passwort geprüft und verglichen, sind beide richtig gibt es true zurück.
+UserSchema.methods.authenticate = async function (plainPassword) {
+  return await bcrypt.compare(plainPassword, this.password);
+};
+
+//Hook: wird vorher ausgeführt und das passwort wird gehasht
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
+});
+
+export const UserModel = model("User", UserSchema);
