58576905a0145de0ce999b4da2145618ae0a9a0c
Patrick <patrick.roessler@dci-student.org>
2024-09-24 11:51:24 +0200

    update

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,4 +1,5 @@
 import Post from "../models/post.models.js";
+
 // POST
 export async function createPost(req, res) {
   try {
@@ -9,24 +10,26 @@ export async function createPost(req, res) {
       author,
     });
     await newPost.save();
-    res.status(201).json({ message: "Post créé avec succès", post: newPost });
+    res.status(201).json({ message: "Post created successfully", post: newPost });
   } catch (error) {
-    res.status(500).json({ message: "Erreur lors de la création du post", error });
+    res.status(500).json({ message: "Error while creating the post", error });
   }
 }
-//DELETE
+
+// DELETE
 export async function deletePost(req, res) {
   try {
     const { postId } = req.params;
     const deletedPost = await Post.findByIdAndDelete(postId);
     if (!deletedPost) {
-      return res.status(404).json({ message: "Post non trouvé" });
+      return res.status(404).json({ message: "Post not found" });
     }
-    res.status(200).json({ message: "Post supprimé avec succès" });
+    res.status(200).json({ message: "Post deleted successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Erreur lors de la suppression du post", error });
+    res.status(500).json({ message: "Error while deleting the post", error });
   }
 }
+
 // PATCH
 export async function updatePost(req, res) {
   try {
@@ -34,10 +37,10 @@ export async function updatePost(req, res) {
     const { title, content } = req.body;
     const updatedPost = await Post.findByIdAndUpdate(postId, { title, content }, { new: true });
     if (!updatedPost) {
-      return res.status(404).json({ message: "Post non trouvé" });
+      return res.status(404).json({ message: "Post not found" });
     }
-    res.status(200).json({ message: "Post mis à jour avec succès", post: updatedPost });
+    res.status(200).json({ message: "Post updated successfully", post: updatedPost });
   } catch (error) {
-    res.status(500).json({ message: "Erreur lors de la mise à jour du post", error });
+    res.status(500).json({ message: "Error while updating the post", error });
   }
 }
